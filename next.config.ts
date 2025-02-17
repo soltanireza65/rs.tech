318226e5c90af5b7d207e82a51d2269b75d66e45
Reza Soltani <soltanireza65@gmail.com>
2025-02-17 22:47:11 +0330

    add mdx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["md", "mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export const config = withMDX(nextConfig);
+export default config;
